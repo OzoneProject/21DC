9dea5dc334f4602023a19c5d3680140b5f7d28d2
krati jain <52825605+kratijain1606@users.noreply.github.com>
2020-06-04 17:28:14 +0530

    add maxProfit

---
+++ Day4/maxProfit.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+     
+        
+        int max=0;
+        for(int i=0;i<prices.length;i++)
+        {
+            for(int j=i+1;j<prices.length;j++)
+            {
+                
+                max=Math.max(max,prices[j]-prices[i]);
+            }
+        }
+        return max;
+    }
+}
+
+
+
